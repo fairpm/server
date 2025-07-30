7457776ae1030636b8ee0cb9326ba5ad6759f66c
Ryan McCue <me@ryanmccue.info>
2025-07-30 15:38:58 +0100

    Disallow file mods
    The filesystem isn't writable, so this stops WP from complaining about
    it, and disables the prompts to update files live.
    
    Signed-off-by: Ryan McCue <me@ryanmccue.info>

--- .config/constants.php
+++ .config/constants.php
@@ -1,7 +1,7 @@
 <?php
 
-define( 'HM_BASIC_AUTH_USER', 'openwebff' );
-define( 'HM_BASIC_AUTH_PW', 'wither-nodding-dues-morals-splat' );
+// Disable writing to the filesystem.
+define( 'DISALLOW_FILE_MODS', true );
 
 // GlotPress configuration.
 define( 'GP_URL_BASE', '/' );
