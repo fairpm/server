2d3b4b5d15ca4b199e762e99a2de3c9a85689f10
Ryan McCue <me@ryanmccue.info>
2025-08-18 19:21:32 +0100

    Add AWS SES WP Mail for email (#22)
    Signed-off-by: Ryan McCue <me@ryanmccue.info>

--- .config/constants.php
+++ .config/constants.php
@@ -6,3 +6,7 @@ define( 'DISALLOW_FILE_MODS', true );
 // GlotPress configuration.
 define( 'GP_URL_BASE', '/' );
 define( 'GP_TMPL_PATH', dirname( __DIR__ ) . '/content/plugins/f-translate/templates/' );
+
+// SES configuration.
+define( 'AWS_SES_WP_MAIL_REGION', 'us-east-1' );
+define( 'AWS_SES_WP_MAIL_USE_INSTANCE_PROFILE', true );
