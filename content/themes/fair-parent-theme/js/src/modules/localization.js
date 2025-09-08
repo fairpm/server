d4c70a0e4df56ee9ab55eee976c81eae767fe79d
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2025-09-08 15:06:16 -0700

    Remove theme and add to git ignore
    Signed-off-by: Mika Ipstenu Epstein <ipstenu@ipstenu.org>

--- content/themes/fair-parent-theme/js/src/modules/localization.js
+++
@@ -1,8 +0,0 @@
-export default function getLocalization(stringKey) {
-  if (typeof window.fair_parent_screenReaderText === 'undefined' || typeof window.fair_parent_screenReaderText[stringKey] === 'undefined') {
-    // eslint-disable-next-line no-console
-    console.error(`Missing translation for ${stringKey}`);
-    return '';
-  }
-  return window.fair_parent_screenReaderText[stringKey];
-}
