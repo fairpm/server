546dbf8c7e9e0eff98a15b0ba82396f1ddcdb6f9
Namith Jawahar <48271037+namithj@users.noreply.github.com>
2025-09-23 19:47:18 +0530

    Define AE_ROOT for Aspire Explorer packages (#37)
    Signed-off-by: Namith Jawahar <48271037+namithj@users.noreply.github.com>

--- .config/constants.php
+++ .config/constants.php
@@ -10,3 +10,6 @@ define( 'GP_TMPL_PATH', dirname( __DIR__ ) . '/content/plugins/f-translate/templ
 // SES configuration.
 define( 'AWS_SES_WP_MAIL_REGION', 'us-east-1' );
 define( 'AWS_SES_WP_MAIL_USE_INSTANCE_PROFILE', true );
+
+// Root path for URL structure for Packages for Aspire Explorer.
+define( 'AE_ROOT', 'packages/' );
