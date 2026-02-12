f26a36a7641bc48ec1c61751067ff84e84d67d4b
Ipstenu (Mika Epstein) <Ipstenu@users.noreply.github.com>
2026-02-12 11:30:49 -0800

    Refactor Redis configuration in environment and object cache files (#98)
    Signed-off-by: Mika Epstein <ipstenu@halfelf.org>
    Signed-off-by: Mika Ipstenu Epstein <ipstenu@halfelf.org>
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: Ipstenu (Mika Epstein) <Ipstenu@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- content/object-cache.php
+++ content/object-cache.php
@@ -1,3 +1,10 @@
 <?php
 WP_Predis\add_filters();
+
+// Set the host from the environment variable before loading the plugin logic
+if ( getenv( 'REDIS_HOST' ) ) {
+    define( 'WP_REDIS_BACKEND_HOST', getenv( 'REDIS_HOST' ) );
+    define( 'WP_REDIS_PORT', getenv( 'REDIS_PORT' ) ?: 6379 );
+}
+
 require_once __DIR__ . '/plugins/wp-redis/object-cache.php';
