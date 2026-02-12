ad941b310726958138504f7c173ce259cc119612
Ipstenu (Mika Epstein) <Ipstenu@users.noreply.github.com>
2026-02-12 14:32:11 -0800

    Update composer dependencies and refine Redis configuration (#101)
    Signed-off-by: Mika Ipstenu Epstein ipstenu@halfelf.org
    Signed-off-by: Mika Ipstenu Epstein <ipstenu@halfelf.org>
    Signed-off-by: Ipstenu (Mika Epstein) <Ipstenu@users.noreply.github.com>

--- content/object-cache.php
+++ content/object-cache.php
@@ -2,13 +2,12 @@
 WP_Predis\add_filters();
 
 // Set the host from the environment variable before loading the plugin logic
-if ( getenv( 'REDIS_HOST' ) ) {
-	if ( ! defined( 'WP_REDIS_BACKEND_HOST' ) ) {
-		define( 'WP_REDIS_BACKEND_HOST', getenv( 'REDIS_HOST' ) );
-	}
-	if ( ! defined( 'WP_REDIS_PORT' ) ) {
-		define( 'WP_REDIS_PORT', getenv( 'REDIS_PORT' ) ?? 6379 );
-	}
+if ( getenv( 'REDIS_PORT' ) && ! defined( 'WP_REDIS_PORT' ) ) {
+	define( 'WP_REDIS_PORT', getenv( 'REDIS_PORT' ) );
+}
+
+if ( getenv( 'REDIS_HOST' ) && ! defined( 'WP_REDIS_BACKEND_HOST' ) ) {
+    define( 'WP_REDIS_BACKEND_HOST', getenv( 'REDIS_HOST' ) );
 }
 
 require_once __DIR__ . '/plugins/wp-redis/object-cache.php';
